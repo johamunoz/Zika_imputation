7f51befb5e0912bdbf3b3d1a7dd2d74822096f0a
Johanna :D <johamunoza@hotmail.com>
2021-10-13 13:51:20 +0200

    Update 2_Pre_HPC.R

--- 2_Code/2_Pre_HPC.R
+++ 2_Code/2_Pre_HPC.R
@@ -40,22 +40,25 @@ post <- ini$post
 #head(ini$loggedEvents)
 #2.1. Assign imputation method to each variable ----
 meth[ncon] <- "2l.norm" # normal distribution at study level
-meth[c("inf_weight","inf_head_circ_birth","inf_length","zikv_ga")]<-"norm" # Due to convergence problems imputation based on marginal distribution
-meth[ncat] <- "2l.pmm" # predictive mean matching at study level
+meth[c("inf_weight","inf_head_circ_birth")]<-"norm"    #  # Due to convergence problems imputation based on marginal distribution
+
 meth[nbin] <- "2l.bin"#  binomial distribution at study level
-meth[c("corticalatrophy","ventriculomegaly","multiplegest","othabnorm","calcifications")] <- "logreg" # Due to frequent singular fit warning imputation based on marginal distribution
+meth[c("corticalatrophy","ventriculomegaly","multiplegest","othabnorm","calcifications","storch_patho","hydrocephaly","inducedabort")] <- "logreg" # Due to frequent singular fit warning imputation based on marginal distribution
+
+meth[ncat] <- "2l.pmm" # predictive mean matching at study level
+meth[c("inf_length","zikv_ga")]<- "2l.ppm"
 
 #2.2. Post-process the values to constrain normal distributed variables in the range of observable values ----
 post["age"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(14, 50))"
 post["bdeath_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 50))"
-post["zikv_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 65))"
+post["zikv_gan"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 65))"
 post["inf_weight"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(100, 7000))"
 post["inf_head_circ_birth"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(15, 50))"
 post["inf_length"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(18, 60))"
 
+
 #3.3. Set prediction matrix -----
-pred <- quickpred(fdata) # assignation based on pairwise correlation
-pred[, c("bdeath_ga")] <- 0 # as information of prediction is given by nelson allen variable na_b
+pred <- quickpred(data, minpuc = 0.3) # assignation based on pairwise correlaition
 pred[,"studyname"] <- -2 # define the cluster for imputation models at study level.
 pred["studyname", "studyname"] <- 0
 pred[,npred]<-0 #not assign variables without prediction power into the imputation model
