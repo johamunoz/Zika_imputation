52dba6da3792d14558427e60cc59b2d082e397af
Johanna :D <johamunoza@hotmail.com>
2021-10-13 14:09:42 +0200

    Update 2_Pre_HPC.R

--- 2_Code/2_Pre_HPC.R
+++ 2_Code/2_Pre_HPC.R
@@ -26,6 +26,9 @@ infoselec[Additionals==3]$Variable # Included in imputation but not as predictor
 
 ncon<-c("age","bdeath_ga","zikv_ga","inf_weight","inf_head_circ_birth","inf_length") # name of continuous variables
 nncon<-colnames(data)[!colnames(data)%in%ncon] # name of no continuous variables
+data[,(nncon):= lapply(.SD, as.factor), .SDcols = nncon] #convert to factors binomial and categorical variables
+data[,studyname:=as.integer(studyname)] # Convert cluster variable studyname to integer 
+summary(data)
 ncat<-c("educ","ethnicity","gravidity","maritalstat","microcephaly","parity")#name of categorical variables
 nbin<-nncon[!nncon%in%ncat] #name of binary variables
 npred<- c("eclampsia","educ","ethnicity","fet_zikv","flavi_alpha_virus","gestdiab","inf_craniofac_abn_bin","maritalstat","tobacco") #name of no predictors
@@ -38,6 +41,8 @@ pred <- ini$pred
 post <- ini$post
 
 #head(ini$loggedEvents)
+
+summary(data)
 #2.1. Assign imputation method to each variable ----
 meth[ncon] <- "2l.norm" # normal distribution at study level
 meth[c("inf_weight","inf_head_circ_birth")]<-"norm"    #  # Due to convergence problems imputation based on marginal distribution
@@ -51,7 +56,7 @@ meth[c("inf_length","zikv_ga")]<- "2l.ppm"
 #2.2. Post-process the values to constrain normal distributed variables in the range of observable values ----
 post["age"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(14, 50))"
 post["bdeath_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 50))"
-post["zikv_gan"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 65))"
+post["zikv_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 65))"
 post["inf_weight"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(100, 7000))"
 post["inf_head_circ_birth"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(15, 50))"
 post["inf_length"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(18, 60))"
@@ -62,3 +67,14 @@ pred <- quickpred(data, minpuc = 0.3) # assignation based on pairwise correlaiti
 pred[,"studyname"] <- -2 # define the cluster for imputation models at study level.
 pred["studyname", "studyname"] <- 0
 pred[,npred]<-0 #not assign variables without prediction power into the imputation model
+
+
+fun_run<-function(imp,data,pred,meth,post,maxit){
+  seed=imp*363+253  
+  start<-proc.time()
+  micesurv <- mice(data, predictorMatrix = pred,method=meth,post=post, maxit =maxit, m = 1, printFlag = TRUE, seed = seed)
+  end<-proc.time()
+  return(list(micesurv,end-start))}
+
+fun_run(imp=1,data=data,pred=pred,meth=meth,post=post,maxit=3)
+
