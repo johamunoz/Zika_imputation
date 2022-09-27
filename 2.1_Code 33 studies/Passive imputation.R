83c9117b0ea397cdf07c989fc67106242c069da9
jaadamen <89968865+jaadamen@users.noreply.github.com>
2022-09-27 11:41:55 +0200

    Update Passive imputation.R

--- 2.1_Code 33 studies/Passive imputation.R
+++ 2.1_Code 33 studies/Passive imputation.R
@@ -66,13 +66,106 @@ data[data=="NA"] <-NA
 #child microcephaly
 
 #Neuroimaging abnormalities other than microcephaly
-col1<-c("fet_us_cns_tri2","fet_us_cns_tri3","ch_hydrocephaly","ch_corticalatrophy","ch_calcifications","ch_ventriculomegaly")
-data[,neuroabnormality:=checkcon(data=data,col1=col1)]
+data$neuroabnormality<-NA
+data$neuroabnormality<-ifelse(data$fet_us_cns_tri2==1 | data$fet_us_cns_tri3==1 | data$ch_hydrocephaly==1 |
+                                data$ch_corticalatrophy==1 | data$ch_calcifications==1 | data$ch_ventriculomegaly==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$fet_us_cns_tri2==0 | data$fet_us_cns_tri3==0 | data$ch_hydrocephaly==0 |
+                    data$ch_corticalatrophy==0 | data$ch_calcifications==0 | data$ch_ventriculomegaly==0,0,NA)
+data$neuroabnormality<-ifelse(is.na(data$neuroabnormality) & data$temp==0,0,data$neuroabnormality)
 
+#Ocular abnormalities
+data$ocularabnormality<-NA
+data$ocularabnormality<-ifelse(data$fet_us_eyeear_tri2==1 | data$fet_us_eyeear_tri3==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$fet_us_eyeear_tri2==0 | data$fet_us_eyeear_tri3==0,0,NA)
+data$ocularabnormality<-ifelse(is.na(data$ocularabnormality) & data$temp==0,0,data$ocularabnormality)
 
+#Congenital contractures
+data$contractures<-NA
+data$contractures<-ifelse(data$fet_us_msk_tri2==1 | data$fet_us_msk_tri3==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$fet_us_msk_tri2==0 | data$fet_us_msk_tri3==0,0,NA)
+data$contractures<-ifelse(is.na(data$contractures) & data$temp==0,0,data$contractures)
+
+#Other nonneurologic abnormalities
+data$nonneurologic<-NA
+data$nonneurologic<-ifelse(data$fet_us_cardio_tri2==1 | data$fet_us_gastro_tri2==1 | data$fet_us_orofac_tri2==1 |
+                             data$fet_us_genur_tri2==1 | data$fet_us_cardio_tri3==1 | data$fet_us_gastro_tri3==1 |
+                             data$fet_us_orofac_tri3==1 | data$fet_us_genur_tri3==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$fet_us_cardio_tri2==0 | data$fet_us_gastro_tri2==0 | data$fet_us_orofac_tri2==0 |
+                    data$fet_us_genur_tri2==0 | data$fet_us_cardio_tri3==0 | data$fet_us_gastro_tri3==0 |
+                    data$fet_us_orofac_tri3==0 | data$fet_us_genur_tri3==0,0,NA)
+data$nonneurologic<-ifelse(is.na(data$nonneurologic) & data$temp==0,0,data$nonneurologic)
+
+#BMI
+data$bmi<-data$pre_pregweight/((data$height/100)**2)
+
+#Maternal teratogenic drug use
+
+#Maternal vaccination
+data$vaccination<-NA
+data$vaccination<-ifelse(data$vac_rub==1 | data$vac_vari==1 | data$vac_yf==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$vac_rub==0 | data$vac_vari==0 | data$vac_yf==0,0,NA)
+data$vaccination<-ifelse(is.na(data$vaccination) & data$temp==0,0,data$vaccination)
+
+#Genetic anomalies
+data$gen_anomalies<-NA
+data$gen_anomalies<-ifelse(data$chromoabn_dx==1 | data$ch_chromoabn==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$chromoabn_dx==0 | data$ch_chromoabn==0,0,NA)
+data$gen_anomalies<-ifelse(is.na(data$gen_anomalies) & data$temp==0,0,data$gen_anomalies)
+
+#Viral load
+data$zikv_pcr_vl_1<-as.numeric(data$zikv_pcr_vl_1)
+
+#Concurrent or prior dengue virus
+data$denv_preg_ever<-NA
+data$denv_preg_ever<-ifelse(data$denv_preg==1 | data$denv_ever==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$denv_preg==0 | data$denv_ever==0,0,NA)
+data$denv_preg_ever<-ifelse(is.na(data$denv_preg_ever) & data$temp==0,0,data$denv_preg_ever)
+
+#Concurrent or prior chik virus
+data$chikv_preg_ever<-NA
+data$chikv_preg_ever<-ifelse(data$chikv_preg==1 | data$chikv_ever==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$chikv_preg==0 | data$chikv_ever==0,0,NA)
+data$chikv_preg_ever<-ifelse(is.na(data$chikv_preg_ever) & data$temp==0,0,data$chikv_preg_ever)
+
+#Pregnancy related comorbidities
+data$comorbid_preg<-NA
+data$comorbid_preg<-ifelse(data$pregcomp_bin==1 | data$gestdiab==1 | data$eclampsia==1 |
+                             data$preeclampsia==1,1,NA)
+data$temp<-NA
+data$temp<-ifelse(data$pregcomp_bin==0 | data$gestdiab==0 | data$eclampsia==0 |
+                    data$preeclampsia==0,0,NA)
+data$comorbid_preg<-ifelse(is.na(data$comorbid_preg) & data$temp==0,0,data$comorbid_preg)
 
 #Variables that we finally need:
-#Exposures: zikv_preg, fet_zikv, zikv_test_ev
+#Exposures: zikv_preg, fet_zikv, zikv_test_ev, zikv_ga
 #Primary outcomes: bdeath, bdeath_ga, fet_micro, ch_czs, czsn
-#Secondary fetal outcomes: igr_curr_preg
-#Secondary infant outcomes: ch_microcephaly_bin, ch_weight, ch_craniofac_abn_bin
+#Secondary fetal outcomes: igr_curr_prg
+#Secondary infant outcomes: ch_microcephaly_bin, ch_weight, ch_craniofac_abn_bin,
+#neuroabnormality, ocularabnormality, contractures, nonneurologic
+#Confounders: age, educ, maritalstat,ethnicity, bmi, ses, tobacco, drugs_bin, alcohol,
+#vaccination, gen_anomalies, zikv_pcr_vl_1, denv_preg_ever, chikv_preg_ever,
+#comorbid_bin, comorbid_preg, storch_bin, arb_symp, fever, rash, arthralgia, headache,
+#muscle_pain, arthritis, vomiting, abd_pain, bleed, fatigue, sorethroat
+
+
+data2<-subset(data, select=c(zikv_preg,fet_zikv, zikv_ga, ch_czs,igr_curr_prg, ch_weight, ch_craniofac_abn_bin,
+                             neuroabnormality, ocularabnormality, contractures, nonneurologic,
+                             age, educ, maritalstat,ethnicity, bmi, ses, tobacco, drugs_bin, alcohol,
+                             vaccination, gen_anomalies, zikv_pcr_vl_1, denv_preg_ever, chikv_preg_ever,
+                             comorbid_bin, comorbid_preg, storch_bin, arb_symp, fever, rash, arthralgia, headache,
+                             muscle_pain, arthritis, vomiting, abd_pain, bleed, fatigue, sorethroat))
+#Change to numeric
+data2<-data2 %>% mutate_if(is.character,as.numeric)
+#sapply(data2, class)
+
+#Not yet included in subset: zikv_test_ev, bdeath, bdeath_ga,fet_micro, czsn, ch_microcephaly_bin
+
+summary(data2)
