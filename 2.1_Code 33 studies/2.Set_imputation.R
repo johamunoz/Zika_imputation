f520bf5cfaa23f8156b45bbfbef34bbc56743c97
johamunoz <johamunoza@hotmail.com>
2022-10-20 19:48:01 +0200

    update 10 imputations
    @ Anneke Please run 2.Set.imputation.R

--- 2.1_Code 33 studies/2.Set_imputation.R
+++ 2.1_Code 33 studies/2.Set_imputation.R
@@ -1,6 +1,6 @@
 #Aim: To set up the imputation methods
 
-rm(list=ls()) # clean environment
+#rm(list=ls()) # clean environment
 
 # Load packages ---
 # Data manipulation package
@@ -16,77 +16,101 @@ library(miceadds)
 
 load(here('3_Output_data','finaldata33.RData'))
 add_info <- as.data.table(readxl::read_xlsx(here('1_Input_data','MasterCodebook_October.xlsx'),sheet="237 key")) #CSV file with the
-add_infoi<-add_info[order(Orderimp)]
+imp_info<-add_info[Final_imputation=="yes"]
+data <- fdata
 
-
-base.dir <- dirname(dirname(rstudioapi::getActiveDocumentContext()$path))
-setwd(base.dir)
-load("3_Output_data/finaldata.RData")
-infoselec<-as.data.table(read.csv2("5_Internal_support/Infoselection.csv",sep=";", stringsAsFactors=FALSE, fileEncoding="latin1"))
-
-infoselec<-infoselec[Imputation2>=3,] #only variable selected
-infoselec[Vtype=="C"]$Variable #Continuous
-infoselec[Vtype=="CAT"]$Variable #Categorical
-infoselec[Additionals==3]$Variable # Included in imputation but not as predictors
-
-
-data<-as.data.table(fdata)
-colnames(data)
-
-
-ncon<-c("age","bdeath_ga","zikv_ga","inf_weight","inf_head_circ_birth","inf_length") # name of continuous variables
-nncon<-colnames(data)[!colnames(data)%in%ncon] # name of no continuous variables
+# Assure format of variables
+nncon<-imp_info[!Type_var%in%c("Continuous","ID")]$who_name
 data[,(nncon):= lapply(.SD, as.factor), .SDcols = nncon] #convert to factors binomial and categorical variables
-data[,studyname:=as.integer(studyname)] # Convert cluster variable studyname to integer 
-summary(data)
 
-ncat<-c("educ","ethnicity","gravidity","maritalstat","microcephaly","parity","tobacco")#name of categorical variables
-nbin<-nncon[!nncon%in%c(ncat,"studyname","Comb_id")] #name of binary variables
-npred<- c("eclampsia","educ","ethnicity","fet_zikv","flavi_alpha_virus","gestdiab","inf_craniofac_abn_bin","maritalstat","tobacco") #name of no predictors
+con_var1<-imp_info[Type_var=="Continuous"&Type_imp==1]$who_name
+con_var2<-imp_info[Type_var=="Continuous"&Type_imp==2]$who_name
+bin_var1<-imp_info[Type_var=="Binary"&Type_imp==1]$who_name
+bin_var2<-imp_info[Type_var=="Binary"&Type_imp==2]$who_name
+cat_var1<-imp_info[Type_var=="Categorical"&Type_imp==1]$who_name
+cat_var2<-imp_info[Type_var=="Categorical"&Type_imp==2]$who_name
+npred<-imp_info[npred==1]$who_name
+
+
+# After checking the histogram we decide to impute this variables as pmm
+cat_var1<-c(cat_var1,"zikv_pcr_vl_1")
+cat_var2<-c(cat_var2,"gravidity","end_ga")
 
 
 #2. Set methods, prediction and post arguments----
 ini <- mice(data, maxit = 0)
+ini$loggedEvents
 meth <- ini$meth
 pred <- ini$pred
 post <- ini$post
 
-head(ini$loggedEvents)
 
 #2.1. Assign imputation method to each variable ----
-meth[ncon] <- "2l.norm" # normal distribution at study level
-meth[nbin] <- "2l.bin"#  binomial distribution at study level
-meth[ncat] <- "2l.pmm" # predictive mean matching at study level
+meth[con_var1] <- "norm" # normal distribution at study level
+meth[con_var2] <- "2l.norm" # normal distribution at study level
+meth[bin_var1] <- "logreg" # normal distribution at study level
+meth[bin_var2] <- "2l.bin" # normal distribution at study level
+meth[cat_var1] <- "pmm"#  binomial distribution at study level
+meth[cat_var2] <- "2l.pmm" # predictive mean matching at study level
 
-meth[c("inf_weight","inf_head_circ_birth")]<-"norm"    #  # Due to convergence problems imputation based on marginal distribution
-meth[c("inf_length","zikv_ga")]<- "2l.pmm" #zikv_gan does not work as normal
-meth[c("corticalatrophy","ventriculomegaly","multiplegest","othabnorm","calcifications","storch_patho","hydrocephaly","inducedabort")] <- "logreg" # Due to frequent singular fit warning imputation based on marginal distribution
-meth[c("educ")]<- "pmm"
-meth
 
 #2.2. Post-process the values to constrain normal distributed variables in the range of observable values ----
-post["age"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(14, 50))"
-post["bdeath_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 50))"
-post["zikv_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 65))"
-post["inf_weight"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(100, 7000))"
-post["inf_head_circ_birth"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(15, 50))"
-post["inf_length"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(18, 60))"
+post["zikv_ga"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 46))"
+post["bmi"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 50))"
+post["age"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(14, 55))"
+post["zikv_assay_ga_1"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(0, 50))"
+post["ch_head_circ_birth"] <- "imp[[j]][, i] <- squeeze(imp[[j]][, i], c(15, 55))"
+
 
 
 #3.3. Set prediction matrix -----
 pred <- quickpred(data, minpuc = 0.3) # assignation based on pairwise correlaition
-pred["Comb_id",] <- 0
-pred[,"Comb_id"] <- 0
-pred[,"studyname"] <- -2 # define the cluster for imputation models at study level.
-pred["studyname", "studyname"] <- 0
+pred["childid",] <- 0
+pred[,"childid"] <- 0
+pred[,"studyimp"] <- -2 # define the cluster for imputation models at study level.
 pred[,npred]<-0 #not assign variables without prediction power into the imputation model
-write.csv(pred,file="5_Internal_support/predmatrix_imp2.csv ")
+write.csv(pred,file=here('3_Output_data','predmatrix33.csv'))
+
 
 fun_run<-function(imp,data,pred,meth,post,maxit){
   seed=imp*363+253  
-  start<-proc.time()
+  start_time<-proc.time()
   micesurv <- mice(data, predictorMatrix = pred,method=meth,post=post, maxit =maxit, m = 1, printFlag = TRUE, seed = seed)
-  end<-proc.time()
-  return(list(micesurv,end-start))}
+  end_time<-proc.time()
+  time = as.numeric(difftime(end_time, start_time, units = "mins"))
+  return(list(micesurv,time))}
 
-fun_run(imp=1,data=data,pred=pred,meth=meth,post=post,maxit=2)
+
+#mice0<-list(micesurv,time)
+#save(mice0,file=here('3_Output_data','mice0.Rdata'))
+
+#mice1<-fun_run(imp=1,data=data,pred=pred,meth=meth,post=post,maxit=10)
+#save(mice1,file=here('3_Output_data','mice1.Rdata'))
+
+#mice2<-fun_run(imp=2,data=data,pred=pred,meth=meth,post=post,maxit=10)
+#save(mice2,file=here('3_Output_data','mice2.Rdata'))
+
+#mice3<-fun_run(imp=3,data=data,pred=pred,meth=meth,post=post,maxit=10)
+#save(mice3,file=here('3_Output_data','mice3.Rdata'))
+
+#mice4<-fun_run(imp=4,data=data,pred=pred,meth=meth,post=post,maxit=10)
+#save(mice4,file=here('3_Output_data','mice4.Rdata'))
+
+#mice5<-fun_run(imp=5,data=data,pred=pred,meth=meth,post=post,maxit=10)
+#save(mice5,file=here('3_Output_data','mice5.Rdata'))
+
+mice6<-fun_run(imp=6,data=data,pred=pred,meth=meth,post=post,maxit=10)
+save(mice6,file=here('3_Output_data','mice6.Rdata'))
+
+mice7<-fun_run(imp=7,data=data,pred=pred,meth=meth,post=post,maxit=10)
+save(mice7,file=here('3_Output_data','mice7.Rdata'))
+
+mice8<-fun_run(imp=8,data=data,pred=pred,meth=meth,post=post,maxit=10)
+save(mice8,file=here('3_Output_data','mice8.Rdata'))
+
+mice9<-fun_run(imp=9,data=data,pred=pred,meth=meth,post=post,maxit=10)
+save(mice9,file=here('3_Output_data','mice9.Rdata'))
+
+mice10<-fun_run(imp=10,data=data,pred=pred,meth=meth,post=post,maxit=10)
+save(mice10,file=here('3_Output_data','mice10.Rdata'))
+
