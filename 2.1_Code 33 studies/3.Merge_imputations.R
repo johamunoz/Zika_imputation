c0a66013a02f6244dbd78cf8d86a637931da8d9c
johamunoz <johamunoza@hotmail.com>
2022-10-24 09:54:34 +0200

    Merged files

---
+++ 2.1_Code 33 studies/3.Merge_imputations.R
@@ -0,0 +1,59 @@
+library(mice)
+library(here)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+
+loadRData <- function(fileName){
+  #loads an RData file, and returns it
+  load(fileName)
+  get(ls()[ls() != "fileName"])[[1]]
+}
+
+
+upfunc<-function(path){
+  load(path)
+  output<-output[[1]]
+  }
+
+mergeMice <- function (imp) {
+  merged_imp <- NULL
+  for (n in 1:length(imp)){
+    if (is.null(merged_imp)){
+      merged_imp <- imp[[n]]
+    }else{
+      counter <- merged_imp$m
+      # Update counter
+      merged_imp$m <- merged_imp$m + imp[[n]]$m
+      # Rename chains
+      dimnames(imp[[n]]$chainMean)[[3]] <-sprintf("Chain %d", (counter + 1):merged_imp$m)
+      dimnames(imp[[n]]$chainVar)[[3]] <-sprintf("Chain %d", (counter + 1):merged_imp$m)
+      # Merge chains
+      merged_imp$chainMean <- abind::abind(merged_imp$chainMean,imp[[n]]$chainMean)
+      merged_imp$chainVar <- abind::abind(merged_imp$chainVar, imp[[n]]$chainVar)
+      
+      for (nn in names(merged_imp$imp)){
+        # Non-imputed variables are not in the data.frame format but are null
+        
+        if (!is.null(imp[[n]]$imp[[nn]])){
+          
+          colnames(imp[[n]]$imp[[nn]]) <- (counter + 1):merged_imp$m
+          merged_imp$imp[[nn]] <- cbind(merged_imp$imp[[nn]],imp[[n]]$imp[[nn]])
+          
+        }
+      }
+    }
+  }
+  return(merged_imp)
+}
+
+path<-file.path(here("3_Output_data"))
+list_files <- list.files(path,pattern="^mice*",full.names=TRUE)
+imp <- lapply(list_files, loadRData)
+merged_imp <- mergeMice(imp)
+
+merged_imp$loggedEvents
+plot(merged_imp)
+densityplot(merged_imp)
+
+
