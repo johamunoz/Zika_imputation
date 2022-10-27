2c3967b0198fd0ae1af16a9ec66f5f9435699fa9
johamunoz <johamunoza@hotmail.com>
2022-10-27 09:54:34 +0200

    save first imputation
    save first imputation

--- 2.1_Code 33 studies/3.Merge_imputations.R
+++ 2.1_Code 33 studies/3.Merge_imputations.R
@@ -56,4 +56,6 @@ merged_imp$loggedEvents
 plot(merged_imp)
 densityplot(merged_imp)
 
-
+save(merged_imp, file =here('3_Output_data','merged_imp.RData'))
+data1<-complete(merged_imp,1)
+write.csv(data1, file =here('3_Output_data','data1.csv'))
