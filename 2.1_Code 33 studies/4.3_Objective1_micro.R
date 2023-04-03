714deb794e5d936554edb93aaee4578c0b40e0b8
johamunoz <johamunoza@hotmail.com>
2023-04-03 17:18:38 +0200

    Graphs Absolute risk with total risk

--- 2.1_Code 33 studies/4.3_Objective1_micro.R
+++ 2.1_Code 33 studies/4.3_Objective1_micro.R
@@ -37,7 +37,7 @@ data_raw %<>%
           mutate(across(where(is.character)&-c(childid,microcephaly,zikv_test_ev,source),
                 ~as.numeric(.x)))
 
-str(data_raw)
+
 # Merge both datasets ----
 data_all <- dplyr::bind_rows(data_imp,data_raw)
 
@@ -51,8 +51,8 @@ data_all%<>%
          microcephaly_bin_birth:= ifelse(microcephaly%in%c(0,3),0,ifelse(microcephaly%in%c(1,2),1,NA))) #microcephaly at birth
 
 # Get studynames
-study_info <- as.data.table(readxl::read_xlsx(here('1_Input_data','MasterCodebook_October.xlsx'),sheet="StudyID"))
-data_all <- merge(data_all,study_info%>%select(c("studyimp","studyname")),by="studyimp",all.x=TRUE)
+study_info <- (readxl::read_xlsx(here('1_Input_data','MasterCodebook_October.xlsx'),sheet="StudyID"))
+data_all <- merge(data_all,study_info%>%select(c("studyimp","studyname","Included")),by="studyimp",all.x=TRUE)
 
 
 # Separate dataset according to zikv_preg
@@ -60,14 +60,21 @@ data_zika <- data_all%>%filter(zikv_preg ==1)
 data_nozika <- data_all%>%filter(zikv_preg ==0)
 
 
-forest_plot_study(data=data_all,outcome_name="microcephaly_bin_birth",plottitle = "Microcephaly at birth,all")
-forest_plot_study(data=data_zika,outcome_name="microcephaly_bin_birth",plottitle = "Microcephaly at birth,zika+ mom")
-forest_plot_study(data=data_nozika,outcome_name="microcephaly_bin_birth",plottitle = "Microcephaly at birth,zika- mom")
+forest_plot_study(data=data_all,outcome_name="microcephaly_bin_birth",plottitle = "Microcephaly at birth, all mom")
+forest_plot_study(data=data_zika,outcome_name="microcephaly_bin_birth",plottitle = "Microcephaly at birth, zika+ mom")
+forest_plot_study(data=data_nozika,outcome_name="microcephaly_bin_birth",plottitle = "Microcephaly at birth, zika- mom")
 
-forest_plot_study(data=data_all,outcome_name="zikv_preg",plottitle = "Zika diagnosis in mothers")
+forest_plot_study(data=data_all,outcome_name="microcephaly_bin_postnatal",plottitle = "Microcephaly post-natal")
+forest_plot_study(data=data_all,outcome_name="microcephaly_bin_fet",plottitle = "Microcephaly at fetus state")
 
+forest_plot_study(data=data_all,outcome_name="ch_czs",plottitle = "Child congenital zika")
+forest_plot_study(data=data_all,outcome_name="who_czs",plottitle = "Child congenital zika according to WHO definition")
 
+forest_plot_study(data=data_all,outcome_name="neuroabnormality",plottitle = "Neuro abnormality")
+forest_plot_study(data=data_all,outcome_name="nonneurologic",plottitle = "Non neurologic abnormalities")
 
+forest_plot_study(data=data_all,outcome_name="miscarriage",plottitle = "Miscarriage")
+forest_plot_study(data=data_all,outcome_name="loss",plottitle = "Loss")
 
-
-
+forest_plot_study(data=data_all,outcome_name="efdeath",plottitle = "Ef death")
+forest_plot_study(data=data_all,outcome_name="lfdeath",plottitle = "Lf death")
--- 2.1_Code 33 studies/F_graph.R
+++ 2.1_Code 33 studies/F_graph.R
@@ -4,34 +4,45 @@
 library(tidyr)
 library(purrr)
 library(magrittr)
+library(ggplot2)
+library(ggh4x)
+library(metafor)
 
 #######Functions#######
-logit <- function(x){x<-ifelse(x==0,1e-10,x);x<-ifelse(x==1,1-1e-10,x);log(x/(1-x))} # better remove NA
+logit <- function(x){log(x/(1-x))} # better remove NA
 logit_inv <- function(x){exp(x)/(1+exp(x))}
 
 
-#Function to calculate absolute risk of an outcome per study and per imputed dataset
+# Function to calculate the observed absolute risk, its se and CI (observed, observed_se,lower,upper) and the logit transformed absolute risk and its se (logit_obs, logit_obs_se)
 f_sumarize_data <- function(data){
     alpha <- 0.05  
-    sum_data<-data%>%summarise(obs_n=n(),obs_sum=sum(outcome,na.rm=TRUE),na_obs=sum(is.na(outcome),na.rm=TRUE))
-    ptest = prop.test(sum_data$obs_sum, sum_data$obs_n, conf.level = alpha)
-    sum_data%<>%
-      mutate(  observed = ptest$estimate,
-               lower = ptest$conf.int[[1]],
-               upper = ptest$conf.int[[2]])%>%
-      mutate( observed = ifelse(observed==0,0.001,observed),
-              lower = ifelse(lower==0,0.001,lower),
-              upper = ifelse(upper==0,0.001,upper))%>%
+    sum_data<-data%>%summarise(obs_n = n(),
+                               obs_sum = sum(outcome,na.rm=TRUE),
+                               na_obs = sum(is.na(outcome),na.rm=TRUE),
+                               included =first(Included))
+    ptest = prop.test(sum_data$obs_sum, sum_data$obs_n, conf.level = alpha, correct = TRUE) # exact CI with Yates correction
+    sum_data%<>% 
+      # in the original scale
+      mutate( observed = ptest$estimate,
+              lower =   ptest$conf.int[[1]],
+              upper = pmin(ptest$conf.int[[2]]))%>%
+      mutate( observed_se = (upper-lower)/(2*qnorm(1-alpha/2)), # approx se on the original scale based on the CI
+              observed_cor=pmax(pmin(ptest$estimate,0.999999),0.000001),# observed corrected when  obs=0 or obs=1 to logit transform  
+              lower_cor=pmax(pmin(lower,0.999999),0.000001),
+              upper_cor=pmax(pmin(upper,0.999999),0.000001))%>%          
       # Logit and se for estimate and observed          
-      mutate(logit_obs = logit(observed),
-             logit_obs_se = sqrt((logit(upper)-logit(lower))/(2*qnorm(1-alpha/2)*obs_n)))
+      mutate( logit_obs = logit(observed_cor),
+              logit_obs_se= (logit(upper_cor)-logit(lower_cor))/(2*qnorm(1-alpha/2)))
+              #logit_obs_se = observed_se/(observed_cor*(1-observed_cor)))
+              #logit_obs_se = 1/(obs_n*observed_cor*(1-observed_cor))) 
     return(sum_data)
   }
   
+alpha=0.05
 
-f_studyimp<-function(data, outcome_name) {
+f_study_imp <- function(data, outcome_name) {
   sum_data<- data%>%
-             dplyr::select(outcome=!!outcome_name,studyname,'.imp')%>%
+             dplyr::select(outcome=!!outcome_name,studyname,Included,'.imp')%>%
              nest(data= - c(studyname,'.imp'))%>%
              mutate(obs_sum = map(data,~f_sumarize_data(data=.x)))%>%
              select(-c(data))%>%
@@ -42,91 +53,130 @@ f_studyimp<-function(data, outcome_name) {
 
 #Function to pool the absolute risks per study per imputed dataset using rubins rules
 
-f_pool <- function(data,est,se){
+f_pool <- function(est, logit_est, logit_se){
+  
   alpha <- 0.05
-  pdata <- data%>% dplyr::select(est = !!est, se = !!se)
-  n <- nrow(pdata)  #number observations
-  mu <- mean(pdata$est,na.rm=T) # pool estimate
-  w_var <- mean(pdata$se^2,na.rm=T) # within variance
-  b_var <- var(pdata$est,na.rm=T) # between variance
-  t_var <- w_var + b_var+ b_var/n # total variance
+  logit_est <- logit_est[!is.infinite(logit_est)&!is.na(logit_est)]
+  logit_se <- logit_se[!is.infinite(logit_se)&!is.na(logit_se)]
+  n <- length(logit_est)  #number observations
+  mu <- mean(logit_est, na.rm = T) # pool estimate
+  w_var <- mean(logit_se^2, na.rm = T) # within variance
+  b_var <- var(logit_est, na.rm = T) # between variance
+  t_var <- w_var + b_var + b_var/n # total variance
   t_se <- sqrt(t_var) # total standard error
   r <- (b_var + (b_var / n))/ w_var # relative increase variance due to missing values
   v <- (n - 1) * (1 + r^-1)^2 # degrees of freedom
-  t <- qt(1-alpha/2, v) #t distribution criteria
-  lower <- mu - t_se*t
-  upper <- mu + t_se*t
-  return(data.frame(cbind(mu=mu,lower=lower,upper=upper)))
+  t <- qt(1-alpha/2, v) #t criteria
+  observed <-  mean(est, na.rm = T)
+  lower <- logit_inv(mu - t_se*t)
+  upper <- logit_inv(mu + t_se*t)
+  
+  return(data.frame(cbind(observed = observed, lower = lower, upper = upper, logit_obs = mu, logit_obs_se = t_se)))
 }
 
-f_data_pool <-function(data,inc.outcome) {
+
+f_data_pool <- function(data) {
   data_pool<-data%>%
              filter(.imp!=0)%>%  
              nest(data=-studyname)%>%  
              mutate(pool_sum=map(data,function(x){
                imp_n = nrow(x)
-               p_obs=f_pool(data=x,est="logit_obs",se="logit_obs_se")
-               observed = logit_inv(p_obs$mu)
-               lower = logit_inv(p_obs$lower)
-               upper = logit_inv(p_obs$upper)
-               return(cbind(p_obs=p_obs,observed=observed,lower=lower,upper=upper))}))%>%
-             select(-c(data))%>%
+               pool_obs=f_pool(est=x$observed,logit_est=x$logit_obs,logit_se=x$logit_obs_se)}))%>%
+    select(-c(data))%>%
             unnest(col= c(pool_sum))
   return(data_pool)}
 
+# Function to calculate the global absolute risk
+f_data_total <- function(data) {
+  
+  data_pool <- data%>%
+               filter(included==1)
+  
+  fit.rma <- rma(yi=data_pool$logit_obs,
+               sei=data_pool$logit_obs_se,
+               method="REML",
+               test= "knha")
+  
+  
+  total <- data.frame(  studyname="TOTAL",
+                         obs_n = sum(data_pool$obs_n,na.rm = T),
+                         obs_sum = sum(data_pool$obs_sum,na.rm = T),
+                         na_obs = sum(data_pool$na_obs,na.rm = T),
+                         included = sum(data_pool$included,na.rm=T),
+                         observed = logit_inv(as.numeric(fit.rma$beta)),
+                         lower = logit_inv(as.numeric(fit.rma$ci.lb)),
+                         upper = logit_inv(as.numeric(fit.rma$ci.ub)),
+                         observed_se = (logit_inv(as.numeric(fit.rma$ci.ub))- logit_inv(as.numeric(fit.rma$ci.lb)))/(2*qnorm(1-alpha/2)),
+                         observed_cor = NA,
+                         logit_obs = as.numeric(fit.rma$beta),
+                         logit_obs_se = as.numeric(fit.rma$se))
+  
+  datan <- data%>%rows_insert(total)
+  return(datan)}
+  
 
 forest_plot_study<-function(data,outcome_name,plottitle){
-  
   #Calculate the absolute risk of the outcome in every study separate and in every imputed dataset
-  inc.outcome<-f_studyimp(data=data,outcome_name)
+  inc.outcome.ind <- f_study_imp(data=data, outcome_name=outcome_name)
+  
+  #Estimate global for each dataset (raw and imputed using only the included datasets)
+  inc.outcome <- inc.outcome.ind%>%
+                 nest(data=-.imp)%>% 
+                 mutate( tdata = map(data,~f_data_total(data=.x)))%>%
+                 select(-c(data))%>%
+                 unnest(col= c(tdata))
+  
+  # Initial num observations and misssing percentage
+  abs.n <- inc.outcome%>%filter(.imp==0)%>%
+           mutate(N=obs_n,
+                  pmiss=sprintf("%.2f",na_obs/obs_n*100))%>%
+            select("studyname","N","pmiss","included")
+  
+  # For raw dataset
+  abs.outcome.raw <- inc.outcome%>%filter(.imp==0)%>%
+    mutate( source = "Raw")%>%
+    select(c("studyname","observed","lower","upper","logit_obs","logit_obs_se","source"))
   
   
-  #For raw dataset
-  abs.outcome.raw<-inc.outcome%>%filter(.imp==0)%>%select(c("studyname","observed","lower","upper","obs_n","na_obs"))
-  abs.outcome.raw$source<-"Raw"
+  # For imputed datases, pool information with Rubins rules
+   
+   abs.outcome.imp <-f_data_pool(data=inc.outcome)
+   abs.outcome.imp$source <-"Imputed"
+    
   
-  #For imputed datasets
-  #Pool the absolute risks over the imputed datasets, resulting in absolute risks per study
-  abs.outcome.imp<-f_data_pool(data=inc.outcome)%>%select(c("studyname","observed","lower","upper"))
-  abs.outcome.imp$source<-"Imputation"
-  abs.outcome.imp%<>%left_join(abs.outcome.raw%>%select(c("studyname","obs_n","na_obs")),by="studyname")
+  # Merge both datasets
   
-  
-  #Collapse both datasets
-  
-  outcome <- dplyr::bind_rows(abs.outcome.imp, abs.outcome.raw)%>%
-    mutate(mean = observed,
-           se = (upper-lower)/1.96,
-           cint=ifelse(is.na(se), "",
-                       paste0(sprintf("%.2f(%.2f,%.2f)",
-                                      observed, lower, upper))),
-           studyname=paste0(studyname),
-           Nobs=paste0("N=",obs_n,",NA=",na_obs))
-  
-  
-  
+  abs.outcome <- dplyr::bind_rows(abs.outcome.imp, abs.outcome.raw)%>%
+                 left_join(abs.n, by=c('studyname'))%>%
+    mutate(mean = observed*100,
+           clb = lower*100,
+           cub = upper*100)%>%
+    mutate(cint = paste0(sprintf("%.2f(%.2f,%.2f)",mean, clb, cub)),
+           allcint = paste0(studyname," \n N=",N,", %miss=",pmiss))
+           
   
   # Create plot
-  dataplot<-setDT(outcome)[,c("studyname","source","cint","mean","lower","upper","Nobs")]
-  dataplot[,source:=factor(source)]
+  
+  abs.outcome$source <- factor(abs.outcome$source)
   dotcols = c("#a6d8f0","#f9b282")
   barcols = c("#008fd5","#de6b35")
   
-  dataplot[,allcint:=paste(studyname,source,cint,sep="-")] 
-  dataplot<-dataplot[order(studyname,source),]
+
+  dataplot<-abs.outcome[order(abs.outcome$studyname,abs.outcome$source),]
+  laballcint<-unique(dataplot$allcint)
   
-  plot<-ggplot(dataplot, aes(x=allcint, y=mean, ymin=lower, ymax=upper,col=source,fill=source)) + 
+  plot<-ggplot(dataplot, aes(x=cint, y=mean, ymin=clb, ymax=cub,col=source,fill=source)) + 
     geom_linerange(size=2,position=position_dodge(width = 0.5)) +
     geom_point(size=1, shape=21, colour="white", stroke = 0.5,position=position_dodge(width = 0.5)) +
-    facet_grid(studyname~ ., switch = "y",scales="free")+
     scale_fill_manual(values=barcols)+
     scale_color_manual(values=dotcols)+
     xlab("Study name")+ ylab("Absolute risk")+
     ggtitle(plottitle)+
     coord_flip() +
-    scale_x_discrete(breaks=levels(factor(dataplot$allcint)),labels = dataplot$cint)+ 
-    facet_grid(studyname + Nobs~ ., switch = "y",scales="free")+
+    #scale_x_discrete(breaks=levels(factor(dataplot$cint)),labels = dataplot$cint)+ 
+    facet_grid(allcint ~ ., switch = "y",scales="free")+
     theme(strip.placement = "outside")+
     theme(strip.text.y.left = element_text(angle = 0),axis.text.y = element_text(size = 6))
+  
   return(plot)}
                
